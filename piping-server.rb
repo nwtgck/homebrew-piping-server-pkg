f88a2c18fcbf60672492b1b59417dce78b2e2d44
Ryo Ota <nwtgck@nwtgck.org>
2021-12-12 01:46:52 +0900

    1.8.1-1

--- piping-server.rb
+++ piping-server.rb
@@ -4,24 +4,24 @@
 class PipingServer < Formula
   desc "Piping Server"
   homepage "https://github.com/nwtgck/piping-server-pkg"
-  version "1.8.0-1"
+  version "1.8.1-1"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/nwtgck/piping-server-pkg/releases/download/v1.8.0-1/piping-server-pkg-mac-x64.tar.gz"
-    sha256 "5f3dcc90b37fe13fcd3b47fa0f96c066bd44e09180b61133a96b308bd0ef44a1"
+    url "https://github.com/nwtgck/piping-server-pkg/releases/download/v1.8.1-1/piping-server-pkg-mac-x64.tar.gz"
+    sha256 "b8890ac3fdcd2fdf6bc388573d814b4e16f9aece88a781750b52401086ac7ae6"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/nwtgck/piping-server-pkg/releases/download/v1.8.0-1/piping-server-pkg-mac-arm64.tar.gz"
-    sha256 "9cc182c3ddd76482345a130b47cc40546bc7c2a9629f085f53d5b72271181fe7"
+    url "https://github.com/nwtgck/piping-server-pkg/releases/download/v1.8.1-1/piping-server-pkg-mac-arm64.tar.gz"
+    sha256 "d3ded4f37ddbcc45d8fbbc038d7907aa88c5ba3a2798a467e39a9d5d852ffe85"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/nwtgck/piping-server-pkg/releases/download/v1.8.0-1/piping-server-pkg-linuxstatic-x64.tar.gz"
-    sha256 "72e906508aa4a0d4dd99fb7ffac4b8e57f95c50b242e67907fc87cb42aec68da"
+    url "https://github.com/nwtgck/piping-server-pkg/releases/download/v1.8.1-1/piping-server-pkg-linuxstatic-x64.tar.gz"
+    sha256 "f5df603e95d3e83c319ccc71f6a279eacd9a5274d91eb458ae147b9f5920d86d"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/nwtgck/piping-server-pkg/releases/download/v1.8.0-1/piping-server-pkg-linuxstatic-arm64.tar.gz"
-    sha256 "72e906508aa4a0d4dd99fb7ffac4b8e57f95c50b242e67907fc87cb42aec68da"
+    url "https://github.com/nwtgck/piping-server-pkg/releases/download/v1.8.1-1/piping-server-pkg-linuxstatic-arm64.tar.gz"
+    sha256 "f5df603e95d3e83c319ccc71f6a279eacd9a5274d91eb458ae147b9f5920d86d"
   end
 
   def install
